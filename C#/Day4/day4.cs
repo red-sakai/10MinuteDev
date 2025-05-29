98a60d4ff103e8e697175d444336de4c7b6ac4ed
red-sakai <jheredmiguelcrepublica@iskolarngbayan.pup.edu.ph>
2025-05-29 21:19:47 +0800

    feat: complete day 4

--- C#/Day4/day4.cs
+++ C#/Day4/day4.cs
@@ -6,6 +6,63 @@ namespace Day4
     {
         static void Main(string[] args)
         {
+            /*
+            Typecasting methods:
+            - Convert.ToInt32()
+            - Convert.ToDouble()
+            - Convert.ToSingle()
+            - Convert.ToDecimal()
+            - Convert.ToBoolean()
+            - Convert.ToString()
+            - Convert.ToChar()
+            - Convert.ToInt64()
+            - Convert.ToInt16()
+            - Convert.ToByte()
+            */
+
+            
+            // Addition
+            System.Console.WriteLine("Input an integer 1: ");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            System.Console.WriteLine("Input an integer 2: ");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+            System.Console.WriteLine("The answer is: " + (number1 + number2));
+            
+
+            
+            // Subtraction
+            System.Console.WriteLine("Input an integer 1: ");
+            int number3 = Convert.ToInt32(Console.ReadLine());
+            System.Console.WriteLine("Input an integer 2: ");
+            int number4 = Convert.ToInt32(Console.ReadLine());
+            System.Console.WriteLine("The answer is: " + (number3 - number4));
+            
+
+            
+            // Multiplication
+            System.Console.WriteLine("Input an integer 1: ");
+            int number5 = Convert.ToInt32(Console.ReadLine());
+            System.Console.WriteLine("Input an integer 2: ");
+            int number6 = Convert.ToInt32(Console.ReadLine());
+            System.Console.WriteLine("The answer is: " + (number5 * number6));
+            
+
+            
+            // Division
+            System.Console.WriteLine("Input an integer 1: ");
+            int number7 = Convert.ToInt32(Console.ReadLine());
+            System.Console.WriteLine("Input an integer 2: ");
+            int number8 = Convert.ToInt32(Console.ReadLine());
+            System.Console.WriteLine("The answer is: " + (number7 / number8));
+            
+
+            
+            // Exponent
+            System.Console.WriteLine("Input an integer 1: ");
+            int number9 = Convert.ToInt32(Console.ReadLine());
+            System.Console.WriteLine("Input an integer 2: ");
+            int number10 = Convert.ToInt32(Console.ReadLine());
+            System.Console.WriteLine("The answer is: " + Math.Pow(number9, number10));
             
         }
     }
