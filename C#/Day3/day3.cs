0a807d3df61260c05f78bae2b7d8e526843c78ce
red-sakai <jheredmiguelcrepublica@iskolarngbayan.pup.edu.ph>
2025-05-29 13:21:35 +0800

    feat: completed day 3

---
+++ C#/Day3/day3.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Day3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+
+            System.Console.WriteLine(name);
+        }
+    }
+}
