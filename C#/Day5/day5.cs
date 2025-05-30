e60d660526422d73bfdb759edaaf4ca23be1af18
red-sakai <jheredmiguelcrepublica@iskolarngbayan.pup.edu.ph>
2025-05-30 17:32:53 +0800

    feat: complete day 5

--- C#/Day5/day5.cs
+++ C#/Day5/day5.cs
@@ -6,7 +6,25 @@ namespace Day5
     {
         static void Main(string[] args)
         {
-            
+            System.Console.WriteLine("How old are you? ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            if (age >= 100)
+            {
+                System.Console.WriteLine("Congratulations, you have lived 10 decades!");
+            }
+            else if (age >= 18)
+            {
+                System.Console.WriteLine("You are an adult with an age of: " + age);
+            }
+            else if (age < 18 && age >= 1)
+            {
+                System.Console.WriteLine("You are a minor with an age of: " + age);
+            }
+            else
+            {
+                System.Console.WriteLine("You aren't even born yet!");
+            }
         }
     }
 }
