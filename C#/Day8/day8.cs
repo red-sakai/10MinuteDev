b0fc8be2ceeaa3651b2a34602903efc7a9bcc620
red-sakai <jheredmiguelcrepublica@iskolarngbayan.pup.edu.ph>
2025-05-31 06:51:17 +0800

    feat: complete day 8

--- C#/Day8/day8.cs
+++ C#/Day8/day8.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Day8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            // normal while loop
+            int i = 0;
+            while (i < 10)
+            {
+                System.Console.WriteLine($"The current number is: {i}");
+                i++;
+            }
+            
+            // do/while loop, this will run the program at least once then verify if the condition is true using the while statement
+            int start = 0;
+            do
+            {
+                System.Console.WriteLine("Enter a number to start from:");
+                try
+                {
+                    start = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (FormatException)
+                {
+                    System.Console.WriteLine("Please input a valid integer.");
+                    continue;
+                }
+            }
+            while (start > 100);
+            // this basically means, inside the do block, it will do it while the condition inside the while statement is true
+
+            while (start < 100)
+            {
+                System.Console.WriteLine($"The current number is: {start}");
+                start++;
+            }
+            
+            // for loop
+            for (int j = 0; j < 20; j = j + 2)
+            {
+                System.Console.WriteLine($"The number is: {j}");
+            }
+            
+        }
+    }
+}
---
+++ C#/Day9/day9.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Day9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // foreach loop, this loop is used to loop through arrays only
+            string[] names = { "Jhered", "Shaikah", "Yves", "Yve" };
+            foreach (string name in names)
+            {
+                System.Console.WriteLine(name);
+            }
+
+            int[] ages = { 18, 19, 20, 18, 18, 18, 19, 23, 24, 20, 26 };
+            foreach (int age in ages)
+            {
+                System.Console.WriteLine(age);
+            }
+        }
+    }
+}
