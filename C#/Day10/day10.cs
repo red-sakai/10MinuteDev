34fca89bfe58f95a5ae96fa50303cb9d2f2fdda4
red-sakai <jheredmiguelcrepublica@iskolarngbayan.pup.edu.ph>
2025-05-31 20:15:56 +0800

    feat: complete day 10

--- C#/Day10/day10.cs
+++ C#/Day10/day10.cs
@@ -6,7 +6,16 @@ namespace Day10
     {
         static void Main(string[] args)
         {
-            
+            // outputs "Jhered"
+            string[] names = { "Jhered", "Trisha", "Rainier", "Jian" };
+            System.Console.WriteLine(names[0]);
+
+            // outputs numbers 1-5 using foreach loop
+            int[] numbers = { 1, 2, 3, 4, 5 };
+            foreach (int number in numbers)
+            {
+                System.Console.WriteLine(number);
+            }
         }
     }
 }
