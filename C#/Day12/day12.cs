ea775f4ec531eebee2e2230f8bfef0f75952b2ac
red-sakai <jheredmiguelcrepublica@iskolarngbayan.pup.edu.ph>
2025-06-01 16:47:19 +0800

    feat: complete day 12

--- C#/Day12/day12.cs
+++ C#/Day12/day12.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Day12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string txt = "Hello World";
+            System.Console.WriteLine(txt.ToUpper());
+            System.Console.WriteLine(txt.ToLower());
+
+            // .IndexOf() returns the index of the specified element and .Substring()
+            // extracts the characters starting from the specified index, it's like
+            // txt[6::]
+            int index = txt.IndexOf("W");
+            string result = txt.Substring(index);
+            System.Console.WriteLine(result);
+        }
+    }
+}
