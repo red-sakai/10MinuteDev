869b625e93b459009708c027c8b9e6fa1ecf5544
red-sakai <jheredmiguelcrepublica@iskolarngbayan.pup.edu.ph>
2025-06-03 10:34:51 +0800

    feat: complete day 16

--- C#/Day16/day16.cs
+++ C#/Day16/day16.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Day16
+{
+    class Car
+    {
+        // Private fields
+        private string _color;
+        private int _maxSpeed;
+
+        // Public properties
+        public int maxSpeed
+        {
+            get { return _maxSpeed; }
+            set { _maxSpeed = value; }
+        }
+        public string Color
+        {
+            get { return _color; }
+            set { _color = value; }
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car_object = new Car();
+            car_object.Color = "red";
+            car_object.maxSpeed = 300;
+            System.Console.WriteLine($"The {car_object.Color} colored car is going {car_object.maxSpeed}mph!!!");
+        }
+    }
+}
