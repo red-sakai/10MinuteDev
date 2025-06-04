6fa74199112ba4c81445ac3dd5a2d1fd0fa023de
red-sakai <jheredmiguelcrepublica@iskolarngbayan.pup.edu.ph>
2025-06-04 21:28:37 +0800

    feat: complete day 17

--- C#/Day17/day17.cs
+++ C#/Day17/day17.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Day17
+{
+    class Car
+    {
+        public string model;
+        public int year;
+        public string color;
+
+        public Car(string model_name, int year_made, string car_color)
+        {
+            model = model_name;
+            year = year_made;
+            color = car_color;
+        }
+
+        static void Main(string[] args)
+        {
+            Car Lamborghini = new Car("Urus", 2020, "Red");
+
+            System.Console.WriteLine($"I have a {Lamborghini.model} that was made in {Lamborghini.year} and has the color {Lamborghini.color}.");
+        }
+    }
+}
