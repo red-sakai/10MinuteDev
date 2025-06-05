e85263c67daae43ca8e6194d7175a9a78d629b55
red-sakai <jheredmiguelcrepublica@iskolarngbayan.pup.edu.ph>
2025-06-05 08:38:24 +0800

    feat: complete day 18

--- C#/Day18/day18.cs
+++ C#/Day18/day18.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Day18
+{
+    class Vehicle
+    {
+        public new string brand;
+
+        public void run()
+        {
+            System.Console.WriteLine($"The {brand} vehicle is running.");
+        }
+    }
+
+    class Car : Vehicle
+    {
+        // constructor
+        public Car()
+        {
+            brand = "Lamborghini";
+        }
+    }
+
+    class Bike : Vehicle
+    {
+        // constructor
+        public Bike()
+        {
+            brand = "BMX";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car my_car = new Car();
+            Bike my_bike = new Bike();
+
+            my_bike.run();
+            my_car.run();
+        }
+    }
+}
