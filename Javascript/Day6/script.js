41997c07e1ab4b1492a4a84746455fa9f51046df
red-sakai <jheredmiguelcrepublica@iskolarngbayan.pup.edu.ph>
2025-07-04 12:38:14 +0800

    feat: finished day 6

---
+++ Javascript/Day6/script.js
@@ -0,0 +1,28 @@
+document.getElementById("submit").onclick = function() {
+    let day = document.getElementById("number").value;
+    switch (day) {
+        case "1":
+            console.log("Monday")
+            break;
+        case "2":
+            console.log("Tuesday")
+            break;
+        case "3":
+            console.log("Wednesday")
+            break;
+        case "4":
+            console.log("Thursday")
+            break;
+        case "5":
+            console.log("Friday")
+            break;
+        case "6":
+            console.log("Saturday")
+            break;
+        case "7":
+            console.log("Sunday")
+            break;
+        default:
+            console.log("Out of range")
+    }
+}
