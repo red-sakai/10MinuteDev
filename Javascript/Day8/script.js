f0da0bc59677d17bf94860b7ad0a06d0a6fe2478
red-sakai <jheredmiguelcrepublica@iskolarngbayan.pup.edu.ph>
2025-07-06 18:28:25 +0800

    feat: finished day 8 js

--- Javascript/Day8/script.js
+++ Javascript/Day8/script.js
@@ -1,11 +1,29 @@
 // while loop
-let username = prompt("What is your name?")
 
-while(username === "" || username === null) {
+// in this scenario it is better to use a do-while loop since a do will always run once until the while condition isn't satisfied,
+// the do is like a finally block in exception handling, regardless of the result, it will always run once
+
+// let username;
+
+// while(username === "" || username === null) {
+//     username = prompt("What is your name?")
+// }
+
+// document.getElementById("name").innerHTML = `Hello ${username}`
+// console.log(`Hello ${username}`)
+
+// do-while loop
+do{
     username = prompt("What is your name?")
-}
+} while(username === "" || username === null)
 
 document.getElementById("name").innerHTML = `Hello ${username}`
 console.log(`Hello ${username}`)
 
 // for loop
+// for(declaration; condition; increment/decrement) {
+//
+//}
+for(let i = 0; i <= 10; i++){
+    console.log(i)
+}
