68dc99518b3b3515b93cdb3258a38f0a34d4de96
red-sakai <jheredmiguelcrepublica@iskolarngbayan.pup.edu.ph>
2025-07-06 18:14:46 +0800

    feat: while loop day 8 js

--- Javascript/Day8/script.js
+++ Javascript/Day8/script.js
@@ -1,11 +1,11 @@
-let sum = 0;
+// while loop
+let username = prompt("What is your name?")
 
-document.getElementById("addButton").onclick = function() {
-    let number = parseFloat(document.getElementById("numbersTypebox").value);
-    sum += number;
-    document.getElementById("numbersTypebox").value = "";
+while(username === "" || username === null) {
+    username = prompt("What is your name?")
 }
 
-document.getElementById("doneButton").onclick = function() {
-    document.getElementById("displayTotal").innerHTML = `The summation is: ${sum}`
-}
+document.getElementById("name").innerHTML = `Hello ${username}`
+console.log(`Hello ${username}`)
+
+// for loop
