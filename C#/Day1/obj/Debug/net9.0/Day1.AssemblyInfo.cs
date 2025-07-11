329829374a33806dfe3b9e72403faca639cca8eb
red-sakai <jheredmiguelcrepublica@iskolarngbayan.pup.edu.ph>
2025-07-11 10:55:36 +0800

    feat: finished day 13

--- C#/Day1/obj/Debug/net9.0/Day1.AssemblyInfo.cs
+++ C#/Day1/obj/Debug/net9.0/Day1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Day1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1976b090349afc3b9d48f7f64f4d3d2e126df1f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9e65459c108db6e3984604d3465320b4437bbb8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Day1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Day1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Javascript/Day13/script.js
@@ -0,0 +1,11 @@
+function hello() {
+    console.log("Hello World!")
+}
+
+function multiply_by_self(num) {
+    return num * num
+}
+
+let result = multiply_by_self(4);
+console.log(result)
+hello();
